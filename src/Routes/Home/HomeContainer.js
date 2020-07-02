11486ff322fab80ae0bb50313b91afc712af211d
baejinnwoo <bjwkor@naver.com>
2020-07-02 20:51:31 +0900

    .

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HomePresenter from "./HomePresenter";
 import {data as dummydata} from "../../ProductData";
 import {gql} from "apollo-boost";
 import { useQuery } from "react-apollo-hooks";
 import { getList } from "../../Queries/readProduct";
+import { API } from "../../api";
 
 
 
 export default (props) => {
     console.log(props)
+
+    useEffect(async () => {
+     const result = await API.getList();
+      console.log(result);
+    }, []);
     const {data,loading} = useQuery(getList,{variables:{
         user_id:31
     }})    
--- src/api.js
+++ src/api.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://211.62.225.216:4001/",
+  baseURL: "https://211.62.225.216:4001",
+  headers:{
+    'Authorization': 'Bearer ' + localStorage.getItem('token'),
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
 });
 
 export const API = {
   getList: () =>
-    api.get("product/total", {
+    api.get("/product/list", {
       method: "POST",
     }),
 };
